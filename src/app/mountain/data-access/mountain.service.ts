eb802c480cf9998dd86a9efd0eec4fd2faf413a6
Luka Peric <Luka.Peric@endava.com>
2023-01-08 17:10:46 +0100

    Added the functionality of Saved button

--- src/app/mountain/data-access/mountain.service.ts
+++ src/app/mountain/data-access/mountain.service.ts
@@ -1,15 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MountainService {
 
+  updatedMountain: any = {}
+  private refetchSubject = new BehaviorSubject(null)
+
   constructor(private http: HttpClient) { }
+
+  get refetch(){
+    return this.refetchSubject.asObservable()
+  }
 
   getSingleMountain(mountainId: number) {
     return this.http.get<any>(`http://localhost:5000/hikend-bb683/us-central1/app/api/mountain/${mountainId}`)
+  }
+
+  toggleSavedMountain(mountainId: number, mountain: any) {
+
+    this.updatedMountain = {
+        info: mountain.info,
+        name: mountain.name,
+        saved: !mountain.saved,
+        hikingClubs: mountain.hikingClubs,
+        yt: mountain.yt, 
+        photo_url: mountain.photo_url,
+        routes: mountain.routes
+    }
+
+    return this.http.put<any>(`http://localhost:5000/hikend-bb683/us-central1/app/api/mountain/update/${mountainId}`, this.updatedMountain).pipe(
+      tap(()=> this.refetchSubject.next(null))
+    )
   }
 
 }
--- src/app/mountain/feature/mountain/mountain.component.ts
+++ src/app/mountain/feature/mountain/mountain.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
 import { MountainService } from '../../data-access/mountain.service';
 
 @Component({
@@ -10,12 +10,20 @@ import { MountainService } from '../../data-access/mountain.service';
 })
 export class MountainComponent implements OnInit {
 
-  mountain$!: Observable<any>
-
+  mountain$!: Observable<any>;
+  mountainId: any = this.route.snapshot.params["id"];
 
   constructor(private mountainService: MountainService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.mountain$ = this.mountainService.getSingleMountain(this.route.snapshot.params["id"])
+    this.mountain$ = this.mountainService.refetch.pipe(
+      switchMap(() => this.mountainService.getSingleMountain(this.mountainId))
+    )
+  }
+
+  toggleSavedMountain(mountain: any) {
+    this.mountainService.toggleSavedMountain(this.mountainId, mountain).subscribe()
   }
 }
+
+
